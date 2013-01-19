7c0d2b41d67a4bcc8943d90e03b775509a14ad59
Alexander Hripak <alex@left-click.us>
2013-01-18 23:21:30 -0500

    nodejs now adds new row to the view

--- demo/sites/all/modules/custom/gif/gif.js
+++ demo/sites/all/modules/custom/gif/gif.js
@@ -13,6 +13,13 @@
 
 	// swap out the old number with the new one
 	$('#number').html(message.data.number);
+
+	// add new row, and reload masonry
+	var view = $(".view-felines.view-display-id-page .view-content");
+	view.prepend(message.data.row);
+	view.imagesLoaded(function() {
+	  view.masonry('reload');
+	});
 	break;
       }
     }
